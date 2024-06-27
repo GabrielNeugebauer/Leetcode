2f3f9fdd1e6c031d27d6c46b999b894d28e634c7
GabrielNeugebauer <gabrielneugebauer@hotmail.com>
2024-06-26 21:22:08 -0300

    Exercise "28_Find_the_Index_of_the_First_Occurrence_in_a_String-Easy.py" concluded with one O(N) method

---
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String-Easy.py
@@ -0,0 +1,11 @@
+class Solution:
+  def strStr(self, haystack: str, needle: str) -> int:
+    #24ms e 16.34MB, O(N)
+    m = len(haystack)
+    n = len(needle)
+
+    for i in range(m - n + 1):
+      if haystack[i:i + n] == needle:
+        return i
+
+    return -1
